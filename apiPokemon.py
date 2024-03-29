6aa67d620c13914d6f7320977de40090dcbb3973
EmmanuelCanto <emmanuel@Emmanuels-MacBook-Air.local>
2024-03-29 17:55:21 -0500

    solo falta solucionar el bucle

--- apiPokemon.py
+++ apiPokemon.py
@@ -1,88 +1,120 @@
-import requests
 
+
+import requests
+import json
 
 class Pokemon:
-
     def __init__(self, nombre):
         self.nombre = nombre
         self.propiedades = requests.get(f"https://pokeapi.co/api/v2/pokemon/{nombre.lower()}")
-        self.respuesta = self.propiedades.json() if self.propiedades.status_code==200 else False
+        self.respuesta = self.propiedades.json() if self.propiedades.status_code == 200 else False
         if self.respuesta:
-            continue:
-        self.id=""
-        self.tipo = ""
-        self.altura = ""
-        self.peso = ""
-    def criatura(self):
-        self.tipo = self.respuesta.get("id",[])
-        self.altura = self.respuesta.get("height",[])
-        self.peso = self.respuesta.get("weight",[])
-        self.peso = self.respuesta.get("weight",[])
-        print("Id: " + str(self.id))
-        print("Tipo: " + str(self.tipo))
-        print("Altura: " + str(self.altura))
-        print("Peso: " + str(self.peso))
+            self.id = self.respuesta["id"]
+            self.altura = self.respuesta["height"]
+            self.peso = self.respuesta["weight"]
+            self.tipo = self.respuesta["types"][0]["type"]["name"]
+            
+    def criaturas(self):
+        if self.respuesta:
+            print("Id:", self.id)
+            print("Tipo:", self.tipo)
+            print("Altura:", self.altura)
+            print("Peso:", self.peso)
+        else:
+            print("No se encontraron propiedades para este Pokémon.")
         
     def debilidades(self):
         
         tabla_tipos = {
-            "Fire": {"Fire": 0.5, "Water": 0.5, "Grass": 2, "Electric": 1},  # Example of weaknesses and resistances of the Fire type
-            "Water": {"Fire": 2, "Water": 0.5, "Grass": 0.5, "Electric": 1},  # Example of weaknesses and resistances of the Water type
-            "Steel": {"Steel": 0.5, "Fighting": 2, "Fire": 1, "Water": 0.5, "Grass": 1, "Flying": 1, "Psychic": 1, "Fairy": 1, "Ground": 2},
-            "Bug": {"Bug": 1, "Fighting": 0.5, "Flying": 2, "Fire": 0.5, "Grass": 2, "Poison": 0.5, "Rock": 2, "Fairy": 0.5},
-            "Dragon": {"Dragon": 1, "Fairy": 0, "Ice": 2, "Dragon": 2, "Steel": 0.5},
-            "Electric": {"Electric": 0.5, "Ground": 2},
-            "Ghost": {"Ghost": 1, "Normal": 0, "Fighting": 0, "Ghost": 2, "Psychic": 2},
-            "Fairy": {"Fairy": 1, "Poison": 0.5, "Steel": 0.5, "Fire": 0.5, "Fighting": 2},
-            "Ice": {"Ice": 0.5, "Fire": 2, "Fighting": 1, "Rock": 1, "Steel": 0.5, "Fairy": 1},
-            "Fighting": {"Fighting": 1, "Flying": 0.5, "Psychic": 0.5, "Fairy": 2, "Bug": 0.5, "Poison": 0.5, "Rock": 2},
-            "Normal": {"Normal": 1, "Fighting": 2},
-            "Grass": {"Grass": 0.5, "Fire": 0.5, "Flying": 0.5, "Ice": 2, "Poison": 0.5, "Ground": 2},
-            "Psychic": {"Psychic": 1, "Bug": 2, "Ghost": 2, "Steel": 0.5},
-            "Rock": {"Rock": 0.5, "Water": 2, "Grass": 2, "Fighting": 0.5, "Ground": 1, "Steel": 1, "Ice": 2},
-            "Dark": {"Dark": 1, "Fighting": 0.5, "Bug": 1, "Fairy": 2, "Ghost": 0, "Psychic": 2},
-            "Ground": {"Ground": 1, "Flying": 0.5, "Ice": 2, "Water": 2, "Grass": 0.5, "Steel": 2},
-            "Poison": {"Poison": 0.5, "Ground": 0.5, "Psychic": 2, "Fairy": 0.5},
-            "Flying": {"Flying": 1, "Electric": 0.5, "Rock": 0.5, "Ice": 2},
+            "fire": {"fire": 0.5, "water": 0.5, "grass": 2, "electric": 1},  # Example of weaknesses and resistances of the Fire type
+            "water": {"fire": 2, "water": 0.5, "grass": 0.5, "electric": 1},  # Example of weaknesses and resistances of the Water type
+            "steel": {"steel": 0.5, "fighting": 2, "fire": 1, "water": 0.5, "grass": 1, "flying": 1, "psychic": 1, "fairy": 1, "ground": 2},
+            "bug": {"bug": 1, "fighting": 0.5, "flying": 2, "fire": 0.5, "grass": 2, "poison": 0.5, "rock": 2, "fairy": 0.5},
+            "dragon": {"dragon": 1, "fairy": 0, "ice": 2, "dragon": 2, "steel": 0.5},
+            "electric": {"electric": 0.5, "ground": 2},
+            "ghost": {"ghost": 1, "normal": 0, "fighting": 0, "ghost": 2, "psychic": 2},
+            "fairy": {"fairy": 1, "poison": 0.5, "steel": 0.5, "fire": 0.5, "fighting": 2},
+            "ice": {"ice": 0.5, "fire": 2, "fighting": 1, "rock": 1, "steel": 0.5, "fairy": 1},
+            "fighting": {"fighting": 1, "flying": 0.5, "psychic": 0.5, "fairy": 2, "bug": 0.5, "poison": 0.5, "rock": 2},
+            "normal": {"normal": 1, "fighting": 2},
+            "grass": {"grass": 0.5, "fire": 0.5, "flying": 0.5, "ice": 2, "poison": 0.5, "ground": 2},
+            "psychic": {"psychic": 1, "bug": 2, "ghost": 2, "steel": 0.5},
+            "rock": {"rock": 0.5, "water": 2, "grass": 2, "fighting": 0.5, "ground": 1, "steel": 1, "ice": 2},
+            "dark": {"dark": 1, "fighting": 0.5, "bug": 1, "fairy": 2, "ghost": 0, "psychic": 2},
+            "ground": {"ground": 1, "flying": 0.5, "ice": 2, "water": 2, "grass": 0.5, "steel": 2},
+            "poison": {"poison": 0.5, "ground": 0.5, "psychic": 2, "fairy": 0.5},
+            "flying": {"flying": 1, "electric": 0.5, "rock": 0.5, "ice": 2},
         }
 
 
-        if self.respuesta.get(["types"][0]["type"]["name"]) in tabla_tipos:
-            return tabla_tipos[self.respuesta.get(["types"][0]["type"]["name"])]
+        if self.tipo in tabla_tipos:
+            print("Debilidades del tipo", self.tipo)
+            for tipo, factor in tabla_tipos[self.tipo].items():
+                print(f"{tipo}: {factor}")
         else:
-            return "Tipo de Pokémon no válido"
+            print("Tipo de Pokémon no válido")
 
     def habilidades(self): 
         if self.respuesta:
             habilidades = self.respuesta.get("abilities", [])
             if habilidades:
+                print("Habilidades:")
                 for habilidad in habilidades:
                     print(habilidad["ability"]["name"])
-            
 
-def menu_informacion_pokemon(Pokemon):
+   
+    def evoluciones(self):
+        query_pokemon = requests.get(f"https://pokeapi.co/api/v2/pokemon-species/{self.nombre.lower()}")
+        if query_pokemon.status_code == 200:
+            json_evoluciones = requests.get(query_pokemon.json()["evolution_chain"]["url"])
+
+            evoluciones = []
+
+            def obtener_recursivo(chain):
+                print("Evoluciones:")
+                if "species" in chain:
+                    evoluciones.append(chain["species"]["name"])
+                    print(chain["species"]["name"])
+                if "evolves_to" in chain and chain["evolves_to"]:
+                    for evolucion in chain["evolves_to"]:
+                        obtener_recursivo(evolucion)
+
+            if "chain" in json_evoluciones.json():
+                obtener_recursivo(json_evoluciones.json()["chain"])
+
+
+        else:
+            print("No se pudieron obtener las evoluciones de este Pokémon.")
+
+
+def menu_informacion_pokemon(pokemon):
     print("1.- Habilidades")
     print("2.- Debilidades")
     print("3.- Propiedades")
     print("4.- Evoluciones")
-    opcion = float(input("Introduce el numero de consulta"))
-    if opcion == 1:
-        Pokemon.habilidades()
-    if opcion == 2:
-        Pokemon.Debilidades()
-    if opcion == 3:
-        Pokemon.criatura()
-        
-        
-    
-    
-# Ejemplo de uso
+    print("5.- Otro pokemon")
+    opcion = input("Introduce el número de consulta: ")
+    if opcion == "1":
+        pokemon.habilidades()
+    elif opcion == "2":
+        pokemon.debilidades()
+    elif opcion == "3":
+        pokemon.criaturas()
+    elif opcion == "4":
+        pokemon.evoluciones()
+    elif opcion == 5:
+        return False
+    else:
+        print("Opción no válida.")
 
 continuar = True
 while continuar:
-    nombrePokemon = input("Introduce el nombre de un Pokémon o 'salir' para terminar el programa > ")
-    if(nombrePokemon.lower()== "salir"):
-        continuar==False
+    nombre_pokemon = input("Introduce el nombre de un Pokémon o 'salir' para terminar el programa: ")
+    if nombre_pokemon.lower() == "salir":
+        continuar = False
     else:
-        miPokemon = Pokemon(nombrePokemon)
-        menu_informacion_pokemon(miPokemon)
+        mismoPokemon = False
+        while mismoPokemon:
+            mi_pokemon = Pokemon(nombre_pokemon)
+            mismoPokemon = menu_informacion_pokemon(mi_pokemon, continuar)
+            
